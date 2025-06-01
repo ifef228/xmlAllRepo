b454b100295791e1c686a385248f2c8206e41a0d
ifef <ifef228@gmail.com>
2025-06-01 11:22:28 +0300

    lab6

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Включаем CORS для всех или укажите origin
+  app.enableCors({
+    origin: '*', // или, например: ['http://localhost:5173']
+    credentials: true,
+  });
+
+  // Статика для бандла
+  app.useStaticAssets(resolve(__dirname, "..",  'public'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
